6d9e7bf6c77cb9b2757f77999f2a7f0ba95686d7
Evgeniy Yakichuk <yak.gjen@gmail.com>
2019-02-08 22:42:13 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import '../assets/js/index';
+
+/*function burgerAnim() {
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('.menu');
+
+  console.log(burger);
+  burger.addEventListener('click', function(event) {
+    burger.classList.toggle('burger-active');
+    menu.classList.toggle('menu-display');
+    setTimeout(() => {
+      menu.classList.toggle('show-menu');
+    }, 0);
+  }, false);
+}*/
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+
+  burgerAnim() {
+    const burger = document.querySelector('.burger');
+    const menu = document.querySelector('.menu');
+
+    burger.addEventListener('click', function(event) {
+      burger.classList.toggle('burger-active');
+      menu.classList.toggle('menu-display');
+      setTimeout(() => {
+        menu.classList.toggle('show-menu');
+      }, 0);
+    }, false);
+  }
+
+  ngOnInit(): void {
+    this.burgerAnim();
+  }
+
 }
---
+++ src/assets/js/index.js
@@ -0,0 +1,11 @@
+/*const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+
+burger.addEventListener('click', function(event) {
+  burger.classList.toggle('burger-active');
+  menu.classList.toggle('menu-display');
+  setTimeout(() => {
+    menu.classList.toggle('show-menu');
+  }, 0);
+  //menu.classList.toggle('show-menu');
+}, false);*/
